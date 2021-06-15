17bf4b0d464707b1832d57432a0749ec14eee8b1
Alizain <alizain.prasla@gmail.com>
2021-06-15 17:28:15 +0500

    Project Setup

---
+++ Binance-MVVM-Combine-SwiftUI/Binance-MVVM-Combine-SwiftUI/Binance_MVVM_Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binance_MVVM_Combine_SwiftUIApp.swift
+//  Binance-MVVM-Combine-SwiftUI
+//
+//  Created by Ali Zain on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Binance_MVVM_Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binance-MVVM-Combine-SwiftUI/Binance-MVVM-Combine-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Binance-MVVM-Combine-SwiftUI
+//
+//  Created by Ali Zain on 15/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
