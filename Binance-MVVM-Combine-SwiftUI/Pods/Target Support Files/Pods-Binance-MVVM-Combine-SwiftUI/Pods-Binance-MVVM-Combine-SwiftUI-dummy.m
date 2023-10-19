b6958ed4dec6cb2ce8928ea81a9b7d8a63de8f75
Alizain Prasla <alizain.prasla@gmail.com>
2023-10-19 13:02:10 +0500

    Deintegrate cocoapods
    deintegrate cocoapods and remove podfile.

--- Binance-MVVM-Combine-SwiftUI/Pods/Target Support Files/Pods-Binance-MVVM-Combine-SwiftUI/Pods-Binance-MVVM-Combine-SwiftUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Binance_MVVM_Combine_SwiftUI : NSObject
-@end
-@implementation PodsDummy_Pods_Binance_MVVM_Combine_SwiftUI
-@end
--- Binance-MVVM-Combine-SwiftUI/Pods/Target Support Files/Pods-Binance-MVVM-Combine-SwiftUITests/Pods-Binance-MVVM-Combine-SwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Binance_MVVM_Combine_SwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Binance_MVVM_Combine_SwiftUITests
-@end
